f53d338c27bedbd3a0c4ed55bf63bb223c38c044
Thomas <tfnielson.se@gmail.com>
2024-07-13 10:36:15 -0600

    /calculate route establised

--- duplocloud_app/app.py
+++ duplocloud_app/app.py
@@ -5,3 +5,23 @@ app = Flask(__name__)
 @app.route("/hello", methods=['GET'])
 def greeting_message():
     return "Hello, Welcome to my Python-Flask App for DuploCloud"
+
+# /calculate?operation=xyz&x=xx&y=yy
+@app.route('/calculate', methods=['GET'])
+def calculate():
+    operation = request.args.get('operation', type=str)
+    x = request.args.get('x', type=float)
+    y = request.args.get('y', type=float)
+
+    if operation == 'add':
+        result = x + y
+    elif operation == 'subtract':
+        result = x - y
+    elif operation == 'multiply':
+        result = x * y
+    elif operation == 'divide':
+        result = x / y
+    else:
+        return "Invalid operation or Missing parameters, please check your query value matches format: /calculate?operation=operation_name&x=x_value&y=y_value - example: /calculate?operation=divide&x=10&y=5"
+
+    return jsonify(result=result)
